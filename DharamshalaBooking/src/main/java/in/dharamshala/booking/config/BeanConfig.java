e0744b5b56fb20a5020e2ad364602881385d5323
Anshul jain <30721082+jain-k-anshul@users.noreply.github.com>
2017-08-13 18:04:14 +0530

    updated

--- DharamshalaBooking/src/main/java/in/dharamshala/booking/config/BeanConfig.java
+++ DharamshalaBooking/src/main/java/in/dharamshala/booking/config/BeanConfig.java
@@ -7,6 +7,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 
+/**
+ * 
+ * @author shwetaJ
+ *
+ */
 @Configuration
 @EnableTransactionManagement
 public class BeanConfig {
